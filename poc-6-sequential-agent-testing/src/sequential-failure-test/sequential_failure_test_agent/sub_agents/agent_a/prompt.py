bd0e90414c20efb84a2a919c8a163d4333ac7763
Thomas Silloway <tcsillow@gmail.com>
2025-05-05 22:28:59 -0400

    Update Agent A prompt so it always runs the tool

--- poc-6-sequential-agent-testing/src/sequential-failure-test/sequential_failure_test_agent/sub_agents/agent_a/prompt.py
+++ poc-6-sequential-agent-testing/src/sequential-failure-test/sequential_failure_test_agent/sub_agents/agent_a/prompt.py
@@ -1,2 +1,6 @@
 # poc6_sequential_failure/sub_agents/agent_a/prompt.py
-AGENT_A_INSTR = """Call the FailingTool. If the tool indicates an error, output a JSON string like '{"status": "failure", "message": "Tool failed: [reason from tool response]"}''. Otherwise, output a JSON string like '{"status": "success", "result": "Tool call completed successfully."}'. Ensure your final output is *only* the valid JSON string."""
+AGENT_A_INSTR = """
+Perform the following steps every time:
+1. Call the FailingTool. 
+2. Analyze the tool response
+3. If the tool indicates an error, output a JSON string like '{"status": "failure", "message": "Tool failed: [reason from tool response]"}''. Otherwise, output a JSON string like '{"status": "success", "result": "Tool call completed successfully."}'. Ensure your final output is *only* the valid JSON string."""
