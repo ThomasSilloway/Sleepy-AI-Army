3e8fd980c6f50aa84dc670eef8d0b6a4ddac01ba
Thomas Silloway <tcsillow@gmail.com>
2025-05-05 22:08:25 -0400

    Add alternate execution options for debug.  The Subagent one is for debugging the sub agents

--- poc-6-sequential-agent-testing/src/sequential-failure-test/sequential_failure_test_agent/prompt.py
+++ poc-6-sequential-agent-testing/src/sequential-failure-test/sequential_failure_test_agent/prompt.py
@@ -4,3 +4,9 @@ Whenever the user says anything, execute the tool called `error_test_tool`.
 Wait for it to return a result. 
 Then, format your final response exactly like this: 'Root Agent reporting: The sequence finished with result: [Insert the result from the tool here]'
 """
+
+ROOT_AGENT_INSTR_SUBAGENT = """
+Transfer to the error_test_sequence subagent. 
+Wait for it to return a result. 
+Then, format your final response exactly like this: 'Root Agent reporting: The sequence finished with result: [Insert the result from the tool here]'
+"""
